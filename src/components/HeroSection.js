ceff04dafb7cb6b7fb138ece3ab9903c36240976
Eremiya Rifat <eremia2021@gmail.com>
2023-01-07 11:44:59 +0100

    asd

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ function HeroSection() {
       <Row className="d-inline">
         <Col className=" text-left">
           <h5 className="text-white bold normalText ">Hallo,mein Name ist</h5>
-          <p className="primaryColor title bold">Eremiya Rifat</p>
+          <p className="primaryColor title bold">Eremiya Rifatooo</p>
           <h4 className="text-white bold biggerNormalText">Ich bin</h4>
           <p className="primaryColor bigTitle bold md-display-1">Webdesigner</p>
         </Col>
